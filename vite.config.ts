c594dcb3ce5ca64bbf774fe3f3432fb396bcb29d
collezionegrenga <collezionegrenga@gmail.com>
2025-07-07 16:03:20 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./",  // <--- QUESTA RIGA RISOLVE IL PROBLEMA DEL "SITO BIANCO"
+  base: "/",  // <--- QUESTA RIGA RISOLVE IL PROBLEMA DEL "SITO BIANCO"
   server: {
     host: "::",
     port: 8080,
